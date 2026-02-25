292a81c74a8141864752ae7947ca704c4c185522
acailly <ylliac14@gmail.com>
2026-02-25 18:43:45 +0100

    feat: add email config

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,14 @@
-module.exports = () => ({});
+module.exports = ({env}) => ({
+  email: {
+    config: {
+      provider: "strapi-provider-email-resend",
+      providerOptions: {
+        apiKey: env("RESEND_API_KEY"),
+      },
+      settings: {
+        defaultFrom: env("RESEND_DEFAULT_FROM_EMAIL"),
+        defaultReplyTo: env("RESEND_DEFAULT_REPLY_TO_EMAIL"),
+      },
+    },
+  },
+});
